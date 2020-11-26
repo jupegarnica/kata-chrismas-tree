c16f3491d4a794aee70f57ec7c3f238a30d98ac1
juan <jupegarnica@gmail.com>
2020-11-26 18:25:22 +0100

    RED stage

--- tree.test.ts
+++ tree.test.ts
@@ -1,13 +1,55 @@
-import { makeTree } from './tree';
+import { makeTree, calcTrunkLength, calcSpaces } from './tree';
 
 describe('Kata christmas tree', () => {
-  test('should work', () => {
+  test('should work for size 2 and 1 char', () => {
     // arrange
-    const expectedResult = 'tree';
+    const expectedResult =
+`
+ *
+***
+ |
+`
+
+// " * \n***\n | "
+
     // action
-    const realResult = makeTree();
+    const realResult = makeTree('*', 2);
 
     // assert
     expect(realResult).toBe(expectedResult);
   });
+  test('should work for size 6 and 1 char', () => {
+    // arrange
+    const expectedResult =
+`
+    *
+   ***
+  *****
+ *******
+*********
+    |
+    |
+`
+
+// " * \n***\n | "
+
+    // action
+    const realResult = makeTree('*', 6);
+
+    // assert
+    expect(realResult).toBe(expectedResult);
+  });
+
+
+  test.only('should calc trunk length correctly', ()=> {
+
+        expect(calcTrunkLength(2)).toBe(1);
+        expect(calcTrunkLength(3)).toBe(1);
+        expect(calcTrunkLength(5)).toBe(1);
+        expect(calcTrunkLength(6)).toBe(2);
+        expect(calcTrunkLength(8)).toBe(2);
+        expect(calcTrunkLength(9)).toBe(3);
+
+
+  })
 });
--- tree.ts
+++ tree.ts
@@ -1,6 +1,15 @@
 
 
 
-export function makeTree() {
+export function makeTree(chars: string, size: number): string {
+    return '';
 
+}
+
+export function calcTrunkLength(size:number):number {
+    return 0;
+}
+
+export function calcSpaces(line:number, size:number):number {
+    return 0;
 }
